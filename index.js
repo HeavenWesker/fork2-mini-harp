9ef549544b12d6284e718ed500a32a7f274d60e9
Heaven Wesker <Heaven.Wesker@gmail.com>
2014-12-04 01:58:15 +0800

    Finished

--- index.js
+++ index.js
@@ -1,12 +1,16 @@
-module.exports = function(){
+module.exports = function(dir){
   var connect = require('connect');
-  var app = connect().use(function(req, res, next){
+  var serveStatic = require('serve-static');
+  var app = connect()
+  .use(function(req, res, next){
+  console.log(req.url)
     if(req.url == '/current-time'){
       res.write((new Date()).toISOString());
       res.end();
     }else{
       next();
     }
-  });
+  })
+  .use(serveStatic(dir))
   return app;
 };
