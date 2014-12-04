fb7ada1b283c5a2b3aa3517eeeed91bb3f4befdf
Heaven Wesker <Heaven.Wesker@gmail.com>
2014-12-04 09:56:14 +0800

    jade

--- bin/mini-harp.js
+++ bin/mini-harp.js
@@ -1,5 +1,6 @@
 #!/usr/bin/env node
 var miniHarp = require('../index.js');
 var argv = require('minimist')(process.argv.slice(2));
-console.log('Listing on '+ argv.p || 400 +'port');
-var app = miniHarp(argv._[0] || process.cwd()).listen(argv.p || 4000);
+var port = argv.p || 4000;
+console.log('Listing on '+ port);
+var app = miniHarp(argv._[0] || process.cwd()).listen(port);
--- index.js
+++ index.js
@@ -1,9 +1,12 @@
-module.exports = function(dir){
+module.exports = function(root){
   var connect = require('connect');
+  var path = require('path')
+  var fs = require('fs')
+  var makeJade = require('./lib/processor/jade.js')
   var serveStatic = require('serve-static');
   var app = connect()
   .use(function(req, res, next){
-  console.log(req.url)
+    console.log(req.url)
     if(req.url == '/current-time'){
       res.write((new Date()).toISOString());
       res.end();
@@ -11,6 +14,7 @@ module.exports = function(dir){
       next();
     }
   })
-  .use(serveStatic(dir))
+  .use(makeJade(root))
+  .use(serveStatic(root))
   return app;
 };
